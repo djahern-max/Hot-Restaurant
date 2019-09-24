f0f51692a8400d074f5f3852e1a057863a6b559d
Dane Ahern <danielaherniv@gmail.com>
2019-09-24 16:15:45 -0400

    update

---
+++ data/tableData.js
@@ -0,0 +1,14 @@
+let tableArray = [
+    {
+        customerName: "Ahmed",
+        customerEmail: "ahmed@example.com",
+        customerID: "afhaque89",
+        phoneNumber: "000-000-0000"
+    }
+];
+
+console.log(tableArray);
+
+module.exports = tableArray;
+
+console.log("test");
